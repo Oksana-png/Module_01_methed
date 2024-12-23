876d3dce930ed2daa71b47a7848a275b4bd5b4c6
Oksana-png <oksana_ovchinnikova@inbox.ru>
2024-12-23 18:21:35 +0500

    create lesson_2

---
+++ lesson_02/script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+{
+  const product = 'Миска';
+  const count = 2;
+  const category = 'Товары для животных';
+  const price = 250;
+
+  console.log(product);
+  console.log(`Сумма: ${count * price}`);
+}
+
+{
+  const product = "Вешалка";
+  const count = 10;
+  const category = "Товары для дома";
+  const price = 190;
+}
